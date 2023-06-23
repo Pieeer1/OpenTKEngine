5c1f0d82dbf54a4d18ccf90db8655af80ef6695d
Pieeer1 <mason.adams@msn.com>
2023-06-22 18:50:24 -0700

    there we go, planes

--- OpenTKEngine/Engine/Window.cs
+++ OpenTKEngine/Engine/Window.cs
@@ -101,7 +101,7 @@ namespace OpenTKEngine.Engine
             lampSphere.AddComponent(new ShapeComponent(Shaders["lampShader"], new Sphere(), new Vector3(-5.0f, -3.0f, -1.5f)));
 
             Entity? plane = _entityComponentManager.AddEntity();
-            plane.AddComponent(new ShapeComponent(Shaders["containerShader"], new Plane(), new Vector3(5.0f, 0.0f, -1.5f)));
+            plane.AddComponent(new ShapeComponent(Shaders["containerShader"], new Plane(10), new Vector3(5.0f, 0.0f, -1.5f)));
             plane.GetComponent<TransformComponent>().RotateTo(new AxisAngle(new Vector3(1.0f, 0.0f, 0.0f), 90.0f));
 
 
--- OpenTKEngine/Models/Shapes3D/Plane.cs
+++ OpenTKEngine/Models/Shapes3D/Plane.cs
@@ -1,28 +1,71 @@
 ﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
 using OpenTKEngine.Entities.Components;
+using OpenTKEngine.Services;
 
 namespace OpenTKEngine.Models.Shapes3D
 {
     public class Plane : Shape3D
     {
-        public override float[] _vertices { get => new float[]
+        private uint _dimensions;
+        public Plane(uint dimensions) 
         {
-            -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
-             0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 0.0f,
-             0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
-             0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
-            -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 1.0f,
-            -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,            
-        }; }
+            _dimensions = dimensions;
+        }
 
+        public override float[] _vertices { get => CreatePlaneVertices(); }
+        public uint[] _indices { get => CreatePlaneIndices(); }
         public override void BindAndBuffer(Shader shader)
         {
-            ArrayBuffer(shader);
+            ElementArrayBuffer(shader, _indices);
         }
 
         public override void Draw(Shader shader, TransformComponent transform)
         {
-            DrawShape(shader, transform, () => GL.DrawArrays(PrimitiveType.Triangles, 0, 36));
+            DrawShape(shader, transform, () => GL.DrawElements(PrimitiveType.Triangles, _indices.Length, DrawElementsType.UnsignedInt, 0));
+        }
+
+        private float[] CreatePlaneVertices()
+        {
+            List<Vector3> positions = new List<Vector3>();
+            List<Vector3> normals = new List<Vector3>();
+            List<Vector2> textures = new List<Vector2>();
+
+            uint halfDim = _dimensions / 2;
+            for (int i = 0; i < _dimensions; i++)
+            {
+                for (int j = 0; j < _dimensions; j++)
+                {
+                    float xSeg = j - halfDim;
+                    float ySeg = 0.0f;
+                    float zSeg = i - halfDim;
+
+                    positions.Add(new Vector3(xSeg, ySeg, zSeg));
+                    normals.Add(new Vector3(xSeg, ySeg, zSeg));
+                    textures.Add(new Vector2(_dimensions, _dimensions));
+                }
+            }
+
+            return DataManipulationService.GetTextureShadedArrayFromVectors(positions, normals, textures).Get1DFrom2D().ToArray();
+        }
+        private uint[] CreatePlaneIndices()
+        {
+            List<uint> indices = new List<uint>();
+            for (int i = 0; i < _dimensions; i++)
+            {
+                for (int j = 0; j < _dimensions; j++)
+                {
+                    indices.Add(_dimensions * (uint)i + (uint)j);
+                    indices.Add(_dimensions * (uint)i + (uint)j + _dimensions);
+                    indices.Add(_dimensions * (uint)i + (uint)j + _dimensions + 1);
+
+                    indices.Add(_dimensions * (uint)i + (uint)j);
+                    indices.Add(_dimensions * (uint)i + (uint)j + _dimensions + 1);
+                    indices.Add(_dimensions * (uint)i + (uint)j + 1);
+
+                }
+            }
+            return indices.ToArray();
         }
     }
 }
