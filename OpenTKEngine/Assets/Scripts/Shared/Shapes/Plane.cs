4b146f1676ada9d4a81fadbdc153fbf8ec94bbc0
Pieeer1 <mason.adams@msn.com>
2023-07-11 19:21:56 -0700

    fixing plane scaling

--- OpenTKEngine/Assets/Scripts/Shared/Shapes/Plane.cs
+++ OpenTKEngine/Assets/Scripts/Shared/Shapes/Plane.cs
@@ -22,14 +22,12 @@ namespace OpenTKEngine.Assets.Scripts.Shared.Shapes
             _rotation = rotation;
             _scale = scale;
 
-            var test = Quaternion.Identity;
+            //Vector3 initialOrientation = new Vector3(0.0f, 0.0f, 1.0f); // default orientation
+            //Vector3 targetOrientation = new Vector3(0.0f, 1.0f, 0.0f);
 
-            Vector3 initialOrientation = new Vector3(0.0f, 0.0f, 1.0f); // default orientation
-            Vector3 targetOrientation = new Vector3(0.0f, 1.0f, 0.0f);
+            //Quaternion rotationQuaternion = Quaternion.FromAxisAngle(Vector3.Cross(initialOrientation, targetOrientation), MathHelper.DegreesToRadians(90f));
 
-            Quaternion rotationQuaternion = Quaternion.FromAxisAngle(Vector3.Cross(initialOrientation, targetOrientation), MathHelper.DegreesToRadians(90f));
-
-            _entity.AddComponent(new ModelComponent(_shader, new Models.Shapes3D.Models.Model($"{AssetRoutes.Models}/plane.dae"), position, rotation ?? rotationQuaternion, scale != null ? new Vector3(scale.Value.X, scale.Value.Y, 0.01f) : new Vector3(1.0f, 1.0f, 0.01f), new List<Texture>()
+            _entity.AddComponent(new ModelComponent(_shader, new Models.Shapes3D.Models.Model($"{AssetRoutes.Models}/plane.dae"), position, rotation, scale != null ? new Vector3(scale.Value.X, 0.01f, scale.Value.Y) : new Vector3(1.0f, 0.01f, 1.0f), new List<Texture>()
             {
                 Texture.LoadFromFile($"{AssetRoutes.Textures}/planegray.png")
             }));
