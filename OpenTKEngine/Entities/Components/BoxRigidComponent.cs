7cfb4fa28d91de729394ec6ac4192db3adbc460f
Pieeer1 <mason.adams@msn.com>
2023-07-14 22:54:12 -0700

    box collider comp and player gravity

---
+++ OpenTKEngine/Entities/Components/BoxRigidComponent.cs
@@ -0,0 +1,21 @@
+﻿using BepuPhysics;
+using BepuPhysics.Collidables;
+using OpenTKEngine.Services;
+
+namespace OpenTKEngine.Entities.Components
+{
+    public class BoxRigidComponent : RigidBodyComponent
+    {
+        private Box _box;
+        public BoxRigidComponent(Box box, float mass) : base(mass, true)
+        {
+            _box = box;
+        }
+        public override void Init()
+        {
+            base.Init();
+            BodyInertia inertia = _box.ComputeInertia(_mass);
+            _handle = PhysicsService.Instance.Simulation.Bodies.Add(BodyDescription.CreateDynamic(DataManipulationService.OpenTKVectorToSystemVector(_transformComponent.Position), inertia, PhysicsService.Instance.Simulation.Shapes.Add(_box), 0.01f));
+        }
+    }
+}
